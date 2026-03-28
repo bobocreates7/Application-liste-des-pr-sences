4c0ab17ec408b53ad5b5864a368354a03f684a85
bobocreates7 <mucobonus2@gmail.com>
2026-03-28 17:30:59 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tournee.appel',
-  appName: 'liste de présence',
+  appName: 'CESCOM LP',
   webDir: 'dist'
 };
 
